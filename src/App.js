d5d371f9eb35da37fe9826b8678e615b79526022
RomanB <djimbik93@gmail.com>
2023-06-18 10:16:03 +0300

    course done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import UsersForm from "./components/form/UsersForm";
+import {useState} from "react";
+import UsersList from "./components/ShowList UI/UsersList";
+import ModalWindow from "./components/modalWindow/ModalWindow";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([
+        {id: 1, name: 'John Walker', age: 29},
+        {id: 2, name: 'Ad Addison', age: 30},
+        {id: 3, name: 'Ars Kimchinskiy', age: 31},
+    ]);
+    const [isModalView, setIsModalView] = useState(false);
+
+    const addUser = (name, age) => {
+        setUsers(prevState => {
+            const newUser = {id: Date.now(), name, age}
+            return [...prevState, newUser]
+        })
+    }
+
+    const deleteUser = (id) => {
+        const userToDeleteIndex = users.findIndex(item => item.id === id)
+        setUsers(prevState => {
+            const newUsers = users.splice(userToDeleteIndex, 1)
+            return [...users]
+        })
+    }
+
+    return (
+        <div className="App">
+            <UsersForm addUser={addUser} setIsModalView={setIsModalView}/>
+            <UsersList deleteUser={deleteUser}
+                       users={users}/>
+            {isModalView && <ModalWindow setIsModalView={setIsModalView}/>}
+        </div>
+    );
 }
 
 export default App;
