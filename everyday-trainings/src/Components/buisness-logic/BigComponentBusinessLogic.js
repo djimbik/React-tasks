d9239709dba6ef75ab59372e85295a6ce66cd4c0
RomanB <djimbik93@gmail.com>
2023-08-06 09:18:12 +0300

    1 task logic done

--- everyday-trainings/src/Components/buisness-logic/BigComponentBusinessLogic.js
+++ everyday-trainings/src/Components/buisness-logic/BigComponentBusinessLogic.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppForBusinessLogic from "./AppForBuisnessLogic";
+import FifthCard from "./FifthCard/FifthCard";
 
 const BigComponentBusinessLogic = () => {
     return (
         <div>
             <AppForBusinessLogic/>
+            <FifthCard/>
         </div>
     );
 };
